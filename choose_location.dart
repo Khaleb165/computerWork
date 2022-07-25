14e4c5ea32fac7ccf3a05ba5bb3d63b056c47c83
khaleb165 <calebagyei165@gmail.com>
2022-07-25 00:17:01 +0000

    add3

--- choose_location.dart
+++ choose_location.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, avoid_print, deprecated_member_use, unused_local_variable
+// ignore_for_file: prefer_const_constructors, avoid_print, deprecated_member_use, unused_local_variable, unused_import
 
 import 'package:flutter/material.dart';
 
@@ -10,29 +10,7 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-  void getData() async {
 
-    //simulate network request for a username
-    String username = await Future.delayed(Duration(seconds: 3), (){
-      return 'caleb';
-    });
-
-    String bio = await Future.delayed(Duration(seconds: 2), (){
-      return 'footballer, drummer and model';
-    });
-
-    print('$username - $bio');
-
-  }
-
-  int counter = 0;
-  @override
-  void initState() {
-    super.initState();
-    getData();
-    print('this will print after the async');
-    print('initState function run');
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,14 +23,7 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 0.0,
       ),
-      body: RaisedButton(
-        onPressed: (){
-          setState(() {
-            counter++;
-          });
-        } ,
-        child: Text('Counter is $counter'),
-      ),
+
     );
   }
 }
--- loading.dart
+++ loading.dart
@@ -1,6 +1,8 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, avoid_print
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -10,6 +12,32 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  void getTime() async {
+
+    Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/Europe/London'));
+    Map data = jsonDecode(response.body);
+    print(data);
+
+    //get properties from data
+    String dateTime = data['datetime'];
+    String offset = data['utc_offset'].substring(1,3);
+   // print(dateTime);
+    //print(offset);
+
+    //create a dateTime object
+    DateTime now = DateTime.parse(dateTime);
+    now = now.add(Duration(hours: int.parse(offset) ));
+    print(now);
+  }
+
+  int counter = 0;
+  @override
+  void initState() {
+    super.initState();
+    getTime();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
